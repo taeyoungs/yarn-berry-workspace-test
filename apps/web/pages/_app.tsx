9b4364fb1ed73b049573a2598c9c67ae089a46df
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-06 21:53:18 +0900

    add: Web 프로젝트에 lib 프로젝트 의존성 주입

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { sayHello } from "@taeyoung/lib";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  sayHello();
+  return <Component {...pageProps} />;
 }
