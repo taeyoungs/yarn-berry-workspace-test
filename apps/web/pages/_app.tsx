b82fe7996f70ec415bbbf4a32ce2458692c61596
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-08 19:10:46 +0900

    mod: sayHello 구문 수정

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { sayHello } from "@taeyoung/lib";
 
 export default function App({ Component, pageProps }: AppProps) {
-  sayHello();
   return <Component {...pageProps} />;
 }
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { sayHello } from "@taeyoung/lib";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2>{sayHello()}</h2>
 
         <p className={styles.description}>
           Get started by editing{" "}
