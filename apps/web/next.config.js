0b7458ef564ba4fc0fe499bccc1dca2c4a255ec6
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-08 19:51:19 +0900

    mod: next-transpile-modules 설정 추가
    next.config.js
    - next-transpile-modules을 통하여 특정 프로젝트의 파일을
      트랜드파일링하도록 Next.js 설정 파일에 옵션을 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,11 @@
+// @wanted/ui 패키지를 tranpile 시킨다.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')(['@taeyoung/ui']);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
